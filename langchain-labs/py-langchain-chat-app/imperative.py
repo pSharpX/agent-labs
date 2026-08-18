9fd3063e38ead90ebf65470631ac45533089e88e
Christian Rivera <crivera2093@gmail.com>
2026-08-18 12:10:38 -0500

    feat: update imperative-based chat.

--- langchain-labs/py-langchain-chat-app/imperative.py
+++ langchain-labs/py-langchain-chat-app/imperative.py
@@ -3,26 +3,34 @@ from langchain_openai.chat_models import ChatOpenAI
 from langchain_core.runnables import chain
 from prompts import template
 
+class ImperativeBasedChatOpenAI:
+    def __init__(self):
+        self.model = ChatOpenAI(model="gpt-3.5-turbo")
 
-model = ChatOpenAI(model="gpt-3.5-turbo")
 
-@chain
-async def chatbot(question: str):
-    """Imperative composition example of langchain components in async way"""
-    prompt = await template.ainvoke({
-        "context": """The most recent advancements in NLP are being driven by Large Language Models (LLMs). 
-            The models outperform their smaller counterparts and have become invaluable for developer who are creating applications with NLP capabilities. 
-            Developers can tap into these models through Hugging Face's `transformers` library, or by utilizing OpenAI and Cohere's offerings through the `openai` and `cohere` libraries, respectively.""",
-        "question": question,
-    })
-    return await model.ainvoke(prompt)
+    def __chatbot(self):
+        @chain
+        async def func(question: str):
+            """Imperative composition example of langchain components in async way"""
+            prompt = await template.ainvoke({
+                "context": """The most recent advancements in NLP are being driven by Large Language Models (LLMs). 
+                    The models outperform their smaller counterparts and have become invaluable for developer who are creating applications with NLP capabilities. 
+                    Developers can tap into these models through Hugging Face's `transformers` library, or by utilizing OpenAI and Cohere's offerings through the `openai` and `cohere` libraries, respectively.""",
+                "question": question,
+            })
+            return await self.model.ainvoke(prompt)
 
-async def main():
-    print("Ask your question: ")
-    question = input()
-    answer = await chatbot.ainvoke(question)
-    print(answer)
+        return func
+
+    async def initialize(self):
+        print("Ask your question: ")
+        question = input()
+        answer = await self.__chatbot().ainvoke(question)
+        print(answer)
+
+
+imperative_chat = ImperativeBasedChatOpenAI()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(imperative_chat.initialize())
 
