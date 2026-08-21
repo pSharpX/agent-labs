f8f9ed3b61d1566922e3ebb082878184b1d8992d
Christian Rivera <crivera2093@gmail.com>
2026-08-20 23:26:50 -0500

    feat: add new contacts operation in service class.

--- langchain-labs/py-langchain-agent/src/contacts/services/contact_service.py
+++ langchain-labs/py-langchain-agent/src/contacts/services/contact_service.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from src.contacts.database import get_db, SessionFactory
+from src.contacts.database import SessionFactory
 from src.contacts.domain.contact import Contact
 
 from src.contacts.repositories.contact_repository import ContactRepository
@@ -12,5 +12,11 @@ class ContactService:
         self.session = SessionFactory()
         self.repo: ContactRepository = SQLAlchemyContactRepository(self.session)
 
-    def get_contact(self, id: UUID) -> Contact | None:
-        return self.repo.get_by_id(contact_id=id)
+    def get_contact(self, contact_id: str) -> Contact | None:
+        return self.repo.get_by_id(contact_id=UUID(contact_id))
+
+    def search_contact(self, name: str) -> list[Contact]:
+        return self.repo.find_by_name(name=name)
+
+    def update_contact(self, contact: Contact) -> Contact:
+        return self.repo.update(contact=contact)
